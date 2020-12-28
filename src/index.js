462e52f6f5ef3fafbbf077685096bf38469d1762
prashant1682 <prashant009@gmail.com>
2020-12-28 11:26:33 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 //It should display 2 paragraph HTML elements.
+const yourName = "Prashant";
+const currentYear = 2021;
+ReactDOM.render(
+  <div>
+    <h1>This is my website.</h1>
+    <p>Created by {yourName}</p>
+    <p>Copy Right {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
 //The paragraphs should say:
 //Created by YOURNAME.
 //Copyright CURRENTYEAR.
